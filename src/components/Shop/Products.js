ed6ed71f3a43a8f810fd878c57335305c3123cc2
Ophelie HORNN <hornn.ophelie@gmail.com>
2023-09-13 16:37:00 +0200

    added list of meals

--- src/components/Shop/Products.js
+++ src/components/Shop/Products.js
@@ -1,16 +1,19 @@
 import ProductItem from './ProductItem';
 import classes from './Products.module.css';
+import meals from "./../utils/meals"
 
 const Products = (props) => {
   return (
     <section className={classes.products}>
       <h2>Buy your favorite products</h2>
       <ul>
-        <ProductItem
-          title='Test'
-          price={6}
-          description='This is a first product - amazing!'
-        />
+        {meals.map(meal =>
+          <ProductItem
+            title={meal.title}
+            price={meal.price}
+            description={meal.description}
+          />
+        )}
       </ul>
     </section>
   );
---
+++ src/components/utils/meals.js
@@ -0,0 +1,29 @@
+const meals = [
+    {
+        title: "Classic Burger",
+        price: 10.99,
+        description: "A juicy beef patty served with lettuce, tomato, onion, and your choice of cheese. Served with crispy fries."
+    },
+    {
+        title: "Vegetarian Pasta",
+        price: 12.49,
+        description: "A delightful pasta dish made with fresh vegetables, garlic, and olive oil. Topped with grated Parmesan cheese."
+    },
+    {
+        title: "Grilled Chicken Salad",
+        price: 9.99,
+        description: "Grilled chicken breast slices on a bed of mixed greens with cherry tomatoes, cucumbers, and balsamic vinaigrette."
+    },
+    {
+        title: "Seafood Platter",
+        price: 16.99,
+        description: "A seafood lover's dream! Includes shrimp, scallops, and cod, lightly breaded and fried. Served with tartar sauce."
+    },
+    {
+        title: "Vegan Buddha Bowl",
+        price: 11.99,
+        description: "A nourishing bowl filled with quinoa, roasted vegetables, chickpeas, and tahini dressing. Vegan and gluten-free."
+    }
+];
+
+export default meals
